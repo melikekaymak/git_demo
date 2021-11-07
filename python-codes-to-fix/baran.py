64f354e2a80ce7e864b0ea04f75b54260bf88676
Kokjix <baranberkbagci@gmail.com>
2021-11-07 22:35:13 +0300

    #1 Function fixed.

--- python-codes-to-fix/baran.py
+++ python-codes-to-fix/baran.py
@@ -1,11 +1,11 @@
 def add_two(a):
-    return a + 7
+    return a + 2
 
 if __name__ == '__main__':
     print("Give the number to be added by two.")
     a = input()
-    result = add_two(a)
-    if(result == a+2):
+    result = add_two(int(a))
+    if(result == int(a)+2):
         print("You fixed it.")
     else:
         print("Result is: ", result, ". Try again.")
